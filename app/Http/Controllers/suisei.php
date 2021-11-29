4f09be803b38db7827f1e09187c027633ae6d4f2
yu-cherry <seaangle66666@gmail.com>
2021-11-29 15:13:48 +0800

    123

---
+++ app/Http/Controllers/suisei.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class suisei extends Controller
+{
+    //
+    public function test()
+    {
+        return 'suiseikawaii';
+
+    }
+}
